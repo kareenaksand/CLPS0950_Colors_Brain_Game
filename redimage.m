8501ddeabe068f6e7acf739deb532258d7601098
smmentor <salina-mari_mentor@brown.edu>
2024-03-15 10:46:51 -0400

    Moving Files

---
+++ redimage.m
@@ -0,0 +1,7 @@
+%test read image
+RedfileID='1rMywuEU3bLoHCFdjeDYpcUM57usf5Pl7';
+RedimageURL= ['https://drive.google.com/uc?id=',RedfileID];
+Redfilename= 'red_image.png';
+websave(Redfilename, RedimageURL);
+RedimageData = imread(Redfilename);
+imshow(RedimageData);
