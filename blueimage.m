50cd713b9c2c736278f62011d7e443b28d34a062
smmentor <salina-mari_mentor@brown.edu>
2024-03-17 12:39:13 -0400

    Full Game Idea

---
+++ blueimage.m
@@ -0,0 +1,14 @@
+BluefileID = '1p0Ne4qO-yW8jpZnGpyHQfGg9s4kwAF-A';
+
+BlueimageURL = ['https://drive.google.com/uc?id=', BluefileID];
+
+Bluefilename = 'blue_image.jpg';
+
+websave(Bluefilename, BlueimageURL);
+
+BlueimageData = imread(Bluefilename);
+
+imshow(BlueimageData);
+!git add .
+!git commit -m "blue image"
+!git push origin main
